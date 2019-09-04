b57ee5189486b1edc16177a77d8382f7e1f9e877
RenanJochem98 <renanjochem98@gmail.com>
2019-09-03 22:52:25 -0300

    Adicionando valor de heuristica

--- Node.py
+++ Node.py
@@ -3,12 +3,19 @@ class Node(object):
     def __init__(self, value, level):
         self.value = value
         self.level = level
+        self.h = 0 # heuristic
 
     def getValue(self):
         return self.value
-    def getLevel(self):
-        return self.level
     def setValue(self, newValue):
         self.value = newValue
+
+    def getLevel(self):
+        return self.level
     def setLevel(self, newLevel):
         self.level = newLevel
+
+    def getH(self):
+        return self.h
+    def setH(self, newH):
+        self.h = newH
