409b3487381e7e3d6670dd052efd7603bba6d225
RenanJochem98 <renanjochem98@gmail.com>
2019-09-03 22:55:42 -0300

    Trocando valor de nivel maximo da matrix e visao detalhada da matrix

--- Utils.py
+++ Utils.py
@@ -2,48 +2,64 @@ import random
 from Node import Node
 class Utils(object):
 
-    def defineGoalMatrix():
+    def __init__(self):
+        self.upperLevel = 3
+
+    def defineGoalMatrix(self):
         goalMatrix = []
-        goalMatrix.append([Node(1,1),Node(2,1),Node(3,1)])
-        goalMatrix.append([Node(1,4),Node(5,2),Node(6,2)])
-        goalMatrix.append([Node(7,3),Node(8,3),Node(0,3)])
+        goalMatrix.append([Node(1,0),Node(2,0),Node(3,0)])
+        goalMatrix.append([Node(4,1),Node(5,1),Node(6,1)])
+        goalMatrix.append([Node(7,2),Node(8,2),Node(0,2)])
         return goalMatrix
 
-    def defineScrambledMatrix():
+    def defineScrambledMatrix(self):
         arr = []
         scrambledMatrix = []
         count = 1
-        level = 1
+        level = 0
         randomValues = random.sample(range(0,9), 9)
         for value in randomValues:
             arr.append(Node(value,level))
-            if count % 3 == 0:
+            if count % self.upperLevel == 0:
                 scrambledMatrix.append(arr)
                 arr = []
                 level+=1
             count+=1
         return scrambledMatrix
 
-    def showIntMatrix(matrix):
+    def showIntMatrix(self, matrix):
         for level in matrix:
             count = 1
             for value in level:
                 endLine = " "
-                if count % 3 == 0:
+                if count % self.upperLevel == 0:
                     endLine = "\n"
                 print(value, end=endLine)
                 count+=1
 
-    def showNodeMatrix(nodeMatrix):
+    def showNodeMatrix(self, nodeMatrix):
+        print ("Visao resumida:")
+        for level in nodeMatrix:
+            count = 1
+            for node in level:
+                endLine = " "
+                if count % self.upperLevel == 0:
+                    endLine = "\n"
+                print(node.getValue(), end=endLine)
+                count+=1
+
+    def showNodeMatrixDetailed(self, nodeMatrix):
+        print ("Visao Detalhada:")
         print ("N = Nodo")
         print ("L = Nivel (Level)")
-        print ("\n")
-        print ("N L   N L   N L")
+        print ("H = Heuristica")
+        print ("")
+        print ("N L H   N L H   N L H")
         for level in nodeMatrix:
             count = 1
             for node in level:
                 endLine = "   "
-                if count % 3 == 0:
+                if count % self.upperLevel == 0:
                     endLine = "\n"
-                print(node.getValue(),node.getLevel(), end=endLine)
+                print(node.getValue(),node.getLevel(),node.getH(), end=endLine)
                 count+=1
