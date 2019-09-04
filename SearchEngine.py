a6ebf1a9c95f377891050fb9beab58791a962e9a
RenanJochem98 <renanjochem98@gmail.com>
2019-09-03 23:00:19 -0300

    Começando busca da heuristica, NAO ESTÁ TERMINADA

--- SearchEngine.py
+++ SearchEngine.py
@@ -4,5 +4,24 @@ class SearchEngine(object):
         self.scrambledMatrix = scrambledMatrix
         self.goalMatrix = goalMatrix
 
+    # Heuristica: Numero de passos ate a posicao correta
     def calculateHeuristic(self):
-        pass
+        scrPosY = 0; #vertical
+        for scrLevel in self.scrambledMatrix:
+            scrPosX = 0; #horizontal
+            for scrNode in scrLevel:
+                value = scrNode.getValue()
+
+
+    def numberSteps(self, value, positionX, positionY):
+        searchedY = -1
+        for level in self.goalMatrix:
+            searchedX = -1
+            searchedY+=1
+            for node in level:
+                searchedX+=1
+                if node.getValue() == value:
+                    break
+            searchedX = -1
+
+        value = (positionX - searchedX)+(positionY - searchedY)
