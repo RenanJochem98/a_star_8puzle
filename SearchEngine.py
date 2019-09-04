364f58e0338be13ff998f524342fe67d973f3a9b
RenanJochem98 <renanjochem98@gmail.com>
2019-09-03 21:35:04 -0300

    Comecando algoritmo de busca

---
+++ SearchEngine.py
@@ -0,0 +1,8 @@
+class SearchEngine(object):
+
+    def __init__(self, scrambledMatrix, goalMatrix):
+        self.scrambledMatrix = scrambledMatrix
+        self.goalMatrix = goalMatrix
+
+    def calculateHeuristic(self):
+        pass
