db0c4c03799d2adc9558f621a46ca70bc6592cfb
RenanJochem98 <renanjochem98@gmail.com>
2019-09-03 21:30:19 -0300

    Adicionando get e set ao Nodo

--- Node.py
+++ Node.py
@@ -1,5 +1,14 @@
 class Node(object):
 
-    def __init__(self):
-        self.value = None
-        self.level = None
+    def __init__(self, value, level):
+        self.value = value
+        self.level = level
+
+    def getValue(self):
+        return self.value
+    def getLevel(self):
+        return self.level
+    def setValue(self, newValue):
+        self.value = newValue
+    def setLevel(self, newLevel):
+        self.level = newLevel
