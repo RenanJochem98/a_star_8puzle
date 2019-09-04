4ae807c99a96a4d04e29f37f658826f5d7152f50
RenanJochem98 <renanjochem98@gmail.com>
2019-09-03 21:32:29 -0300

    Colocando Nodos na matriz inicial e objetivo

--- Utils.py
+++ Utils.py
@@ -1,23 +1,26 @@
 import random
+from Node import Node
 class Utils(object):
 
     def defineGoalMatrix():
         goalMatrix = []
-        goalMatrix.append([1,2,3])
-        goalMatrix.append([4,5,6])
-        goalMatrix.append([7,8,0])
+        goalMatrix.append([Node(1,1),Node(2,1),Node(3,1)])
+        goalMatrix.append([Node(1,4),Node(5,2),Node(6,2)])
+        goalMatrix.append([Node(7,3),Node(8,3),Node(0,3)])
         return goalMatrix
 
     def defineScrambledMatrix():
         arr = []
         scrambledMatrix = []
         count = 1
-        randomValues = random.sample(range(10), 10) # comeca no zero...
+        level = 1
+        randomValues = random.sample(range(0,9), 9)
         for value in randomValues:
-            arr.append(value)
+            arr.append(Node(value,level))
             if count % 3 == 0:
                 scrambledMatrix.append(arr)
                 arr = []
+                level+=1
             count+=1
         return scrambledMatrix
 
@@ -29,4 +32,18 @@ class Utils(object):
                 if count % 3 == 0:
                     endLine = "\n"
                 print(value, end=endLine)
+                count+=1
+
+    def showNodeMatrix(nodeMatrix):
+        print ("N = Nodo")
+        print ("L = Nivel (Level)")
+        print ("\n")
+        print ("N L   N L   N L")
+        for level in nodeMatrix:
+            count = 1
+            for node in level:
+                endLine = "   "
+                if count % 3 == 0:
+                    endLine = "\n"
+                print(node.getValue(),node.getLevel(), end=endLine)
                 count+=1
