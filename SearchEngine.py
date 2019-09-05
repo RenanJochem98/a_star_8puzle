3bf1f9b36c37db0082b452191efdbe4ae754f049
RenanJochem98 <renanjochem98@gmail.com>
2019-09-04 23:50:39 -0300

    Terminando calculo da heuristica de numero de passos

--- SearchEngine.py
+++ SearchEngine.py
@@ -4,24 +4,46 @@ class SearchEngine(object):
         self.scrambledMatrix = scrambledMatrix
         self.goalMatrix = goalMatrix
 
+    def getScrambledMatrix(self):
+        return self.scrambledMatrix
+
+    def setScrambledMatrix(self, newMatrix):
+        self.scrabledmatrix = newMatrix
+
+    def getGoalMatrix(self):
+        return self.goalMatrix
+
     # Heuristica: Numero de passos ate a posicao correta
     def calculateHeuristic(self):
-        scrPosY = 0; #vertical
+        scrPosHoriz = 0; #horizontal
         for scrLevel in self.scrambledMatrix:
-            scrPosX = 0; #horizontal
+            scrPosVert = 0; #vertical
             for scrNode in scrLevel:
-                value = scrNode.getValue()
+                value =scrNode.getValue()
+                numberSteps = self.numberSteps(value, scrPosHoriz, scrPosVert)
+                scrNode.setH(numberSteps)
+                # print(scrPosHoriz, end=" ")
+                # print(scrPosVert, end=" ")
+                # print(value, end=" ")
+                # print(numberSteps)
+                scrPosVert+=1
+            scrPosHoriz+=1
 
-
-    def numberSteps(self, value, positionX, positionY):
-        searchedY = -1
+    def numberSteps(self, value, positionHoriz, positionVert):
+        searched = False
+        searchedHoriz = -1
         for level in self.goalMatrix:
-            searchedX = -1
-            searchedY+=1
+            searchedVert = -1
+            searchedHoriz+=1
             for node in level:
-                searchedX+=1
+                searchedVert+=1
                 if node.getValue() == value:
+                    searched = True
                     break
-            searchedX = -1
+            if searched:
+                break
 
-        value = (positionX - searchedX)+(positionY - searchedY)
+        numberStepsHoriz = abs(searchedHoriz - positionHoriz)
+        numberStepsVert = abs(searchedVert - positionVert)
+        numberSteps = numberStepsHoriz + numberStepsVert
+        return numberSteps
