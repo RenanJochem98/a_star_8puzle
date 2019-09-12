ae26d4997c0570b594483cc5e821d38831c09fe9
RenanJochem98 <renanjochem98@gmail.com>
2019-09-12 12:57:13 -0300

    Mudanças para visualização

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Node import Node
 from Matrix import Matrix
-from SearchEngine import SearchEngine
+# from SearchEngine import SearchEngine
+from BuscaCega import BuscaCega
 
 print("")
 print("#"*30)
@@ -10,22 +11,30 @@ print("\n")
 
 goalMatrix = Matrix('goal')
 scrambledMatrix = Matrix('scrambled')
-searchEngine = SearchEngine(scrambledMatrix, goalMatrix)
+searchEngine = BuscaCega(scrambledMatrix, goalMatrix)
 
-print("Matriz objetivo:")
-goalMatrix.showNodeMatrix()
-print("\n")
+
+print("Matr/iz objetivo:")
+# goalMatrix.showNodeMatrix()
+# print("\n")
 
 print("Matriz Inicial:")
 scrambledMatrix.showNodeMatrix()
 print("\n")
-scrambledMatrix.showNodeMatrixDetailed()
-print("\n")
+# scrambledMatrix.showNodeMatrixDetailed()
+# print("\n")
 
 print("Valores Calculados:")
-searchEngine.calculateHeuristic()
-searchEngine.getScrambledMatrix().showNodeMatrixDetailed()
-print("\n")
+# print(searchEngine.isGoalMatrix(scrambledMatrix))
+# searchEngine.calculateHeuristic()
+# searchEngine.getScrambledMatrix().showNodeMatrixDetailed()
+# print("\n")
+# horiz = int(input("Posicao Horizontal: "))
+# vert = int(input("Posicao Vertical: "))
+# scrambledMatrix.move(horiz, vert)
+# scrambledMatrix.showNodeMatrix()
+# print("\n")
+searchEngine.buscaCega(None)
 
 print("\n")
 print("#"*30)
