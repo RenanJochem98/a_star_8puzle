b198a471489679defdfed7f1cb7ff447dbf1e4cd
RenanJochem98 <renanjochem98@gmail.com>
2019-09-16 00:28:01 -0300

    Adicionando criacao de id e direção do movimenti que leva ao estado

--- State.py
+++ State.py
@@ -1,13 +1,24 @@
 class State(object):
 
-    def __init__(self, newId, parentId, matrix):
-        self.id = newId
-        self.parentId = parentId
+    def __init__(self, parentId, matrix, direction=""):
         self.matrix = matrix
+        self.id = self.createId()
+        self.parentId = parentId
+        self.direction = direction
         self.childs = []
 
+    def createId(self):
+        id = ""
+        for level in self.matrix.getValues():
+            for node in level:
+                id += str(node.getValue())
+        return int(id)
+    def getDirection(self):
+        return self.direction
     def getId(self):
         return self.id
+    def getParentId(self):
+        return self.parentId
     def getMatrix(self):
         return self.matrix
     def addChild(self, childId):
