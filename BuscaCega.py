233840862f47328c6c67bcde211d185b15d5e30d
RenanJochem98 <renanjochem98@gmail.com>
2019-09-16 00:30:23 -0300

    Busca cega encontrando resultado. Falta mostrar o caminho ate ele

--- BuscaCega.py
+++ BuscaCega.py
@@ -1,48 +1,86 @@
+import time
+from datetime import datetime
+import copy
+from Matrix import Matrix
 from State import State
 from SearchEngine import SearchEngine
 class BuscaCega(SearchEngine):
 
     def __init__(self, scrambledMatrix, goalMatrix):
         SearchEngine.__init__(self, scrambledMatrix, goalMatrix)
+        self.stateId = 0
+        self.goalState = None
+        self.caminho = []
 
     def buscaCega(self, state):
+        inicio = datetime.now()
+        print("Inicio:", end="")
+        print(inicio)
         finded = False
         if state == None:
-            state =State(0, -1, self.scrambledMatrix)
+            state =State( -1, self.scrambledMatrix)
             self.states[state.getId()] = state
             self.toVisitStates.append(state)
         count = 0
-        for visitedState in self.toVisitStates:
-            if count > 1000:
+        for currentState in self.toVisitStates:
+            count += 1
+            print("Loop: ", end="")
+            print(count)
+            print("Visitando estado: ", end="")
+            print(currentState.getId())
+            print("Estado Pai: ", end="")
+            print(currentState.getParentId())
+            currentMatrix = currentState.getMatrix()
+            if count > 10:
                 break
-            visitedState.getMatrix().showNodeMatrix()
-            print("\n")
-            if self.isGoalMatrix(visitedState.getMatrix()):
+            if self.isGoalMatrix(currentMatrix):
+                self.goalState = currentState
+                print("Estado "+str(currentState.getId())+ " é final!")
                 finded = True
                 break
-            print("Visitando estado: " + str(visitedState.getId()) )
-            self.visitState(visitedState)
-            count+=1
-        #     del self.toVisitStates[0]
-        # if finded:
-        #     print("Achou!!!!!")
-        #     visitedState.getMatrix().showNodeMatrix()
-        # else:
-        #     print("Nao achou")
+            else:
+                # print("Estado "+str(currentState.getId())+ " não é final!")
+                validMoves = self.getValidMoves(currentMatrix)
+                for move in validMoves:
+                    self.stateId += 1
+                    tempMatrix = copy.deepcopy(currentMatrix)
+                    tempMatrix.moveTo(move)
+                    newState = State( currentState.getId(), tempMatrix)
+                    if newState.getId() not in self.visitedStates:
+                        self.states[newState.getId()] = newState
+                        self.toVisitStates.append(newState)
+                self.visitedStates.append(newState.getId())
+            tempoExec = datetime.now()
+            print("Tempo de Execução: ", end="")
+            print(tempoExec - inicio)
+            currentMatrix.showNodeMatrix()
+            print("\n")
+        print("\n")
+        print("Fim da Busca")
+        print("\n")
+        if finded:
+            print("Achou")
+            print("Numero de estados totais: ", end="")
+            print("Id estado Final: ", end="")
+            print(self.goalState.getId())
+            print("Estado Final: ")
+            self.goalState.getMatrix().showNodeMatrix()
+        else:
+            print("Não achou")
+        print("States: ")
+        print(len(self.states))
+        # for s in self.states:
+        #     print(str(self.states[s].getId()), end=" -> ")
+        print("\n")
+        print("Visited States: ")
+        print(len(self.visitedStates))
+        # for s in self.states:
+        #     print(str(s), end=" -> ")
+        print("\n")
+        print("To Visit States: ")
+        print(len(self.toVisitStates))
 
         print("\n")
-        print("\n")
-        for x in self.visitedStates:
-            print(x)
-
-    def visitState(self, state):
-        currentMatrix = state.getMatrix()
-        validDirections = self.getValidMoves(currentMatrix)
-        for direction in validDirections:
-            currentMatrix.moveTo(direction)
-            newState = State(state.getId()+1, state.getId(), currentMatrix)
-            if newState.getId() not in self.visitedStates:
-                self.states[newState.getId()] = newState
-                self.toVisitStates.append(newState)
-
-        self.visitedStates.append(state.getId())
+        fim = datetime.now()
+        print(fim - inicio)
+        print("Acabou")
