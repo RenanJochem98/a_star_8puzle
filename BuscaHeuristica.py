1d8b194f74dbb41bcef13acb024910cdab942ef3
RenanJochem98 <renanjochem98@gmail.com>
2019-09-17 13:25:39 -0300

    Implementando visita a estados

--- BuscaHeuristica.py
+++ BuscaHeuristica.py
@@ -1,12 +1,36 @@
+import copy
+from State import State
+from SearchEngine import SearchEngine
 class BuscaHeuristica(SearchEngine):
 
     def __init__(self, scrambledMatrix, goalMatrix):
         SearchEngine.__init__(self, scrambledMatrix, goalMatrix)
 
+    def buscaHeuristica(self):
+        self.calculateHeuristic() # calcula heuristica para cada nodo do matriz inicial
+        self.buscaResumida(None, True)
+
+    def visitNode(self, currentState, matrix):
+        validMoves = self.getValidMoves(matrix)
+        newLevel = currentState.getLevel() + 1
+        self.currentLevel = newLevel
+        for move in validMoves:
+            self.stateId += 1
+            tempMatrix = copy.deepcopy(matrix)
+            node = tempMatrix.moveTo(move)
+            newState = State( currentState.getId(), tempMatrix, newLevel, move)
+            newState.setH(node.getH())
+            if newState.getId() not in self.visitedStates:
+                self.states[newState.getId()] = newState
+                self.toVisitStates.append(newState.getId())
+
+        self.visitedStates.append(currentState.getId())
+
+
     # Heuristica: Numero de passos ate a posicao correta
     def calculateHeuristic(self):
         scrPosHoriz = 0; #horizontal
-        for scrLevel in self.scrambledMatrix.getMatrix():
+        for scrLevel in self.scrambledMatrix.getValues():
             scrPosVert = 0; #vertical
             for scrNode in scrLevel:
                 value =scrNode.getValue()
@@ -22,7 +46,7 @@ class BuscaHeuristica(SearchEngine):
     def numberSteps(self, value, positionHoriz, positionVert):
         searched = False
         searchedHoriz = -1
-        for level in self.goalMatrix.getMatrix():
+        for level in self.goalMatrix.getValues():
             searchedVert = -1
             searchedHoriz+=1
             for node in level:
