67dc37bf1b952a760acd1dff42f07c88bd72843d
DEVDIMAS03 <renanjochem98@gmail.com>
2019-09-18 23:39:51 -0300

    Corrigindo ordem da ordenação e exclusão dos estados 'a visitar'

--- SearchEngine.py
+++ SearchEngine.py
@@ -89,16 +89,16 @@ class SearchEngine(object):
                 finded = True
                 break
             else:
-                self.visitNode(currentState, currentMatrix)
+                self.visitNode(currentState, currentMatrix) #deve ser implementa em classe filha
                 del self.toVisitStates[0]
-                currentStateId = self.toVisitStates[0] # novo posicao 0 era posicao 1 antes da delecao
-                if self.currentLevel != currentLevelControl: #current level pode ser alterado no visitNode()
-                    self.addCoustInVisitedStates()
                 if needOrder:
-                    self.toVisitStates.sort(key=lambda x: self.states[x].getOrdered())
-                    for i in self.visitedStates:
-                        self.states[i].setH(0)
-
+                    self.toVisitStates.sort(key=attrgetter("h"))
+                    # for i in self.visitedStates:
+                    #     if self.states[i].getVisited():
+                    #         self.states[i].setH(0)
+                currentState = self.toVisitStates[0] # novo posicao 0 era posicao 1 antes da delecao
+                if self.currentLevel != currentLevelControl: #current level pode ser alterado no visitNode()
+                    self.addCoustInVisitedStates()                
             currentLevelControl = self.currentLevel
             currentMatrix.showNodeMatrix()
             count += 1
@@ -150,16 +150,15 @@ class SearchEngine(object):
                 break
             else:
                 self.visitNode(currentState, currentMatrix) #deve ser implementa em classe filha
-                currentState.setVisited(True)
                 del self.toVisitStates[0]
-                currentStateId = self.toVisitStates[0] # novo posicao 0 era posicao 1 antes da delecao
+                if needOrder:
+                    self.toVisitStates.sort(key=attrgetter("h"))
+                    # for i in self.visitedStates:
+                    #     if self.states[i].getVisited():
+                    #         self.states[i].setH(0)
+                currentState = self.toVisitStates[0] # novo posicao 0 era posicao 1 antes da delecao
                 if self.currentLevel != currentLevelControl: #current level pode ser alterado no visitNode()
                     self.addCoustInVisitedStates()
-                if needOrder:
-                    self.toVisitStates.sort(key=lambda x: self.states[x].getOrdered())
-                    for i in self.visitedStates:
-                        if self.states[i].getVisited():
-                            self.states[i].setH(0)
             currentLevelControl = self.currentLevel
 
         fim = datetime.now()
