3ae4b3c9f792e823ccb01c29854acb9844d52f6f
RenanJochem98 <renanjochem98@gmail.com>
2019-09-23 12:51:28 -0300

    Adicionando escolha do tipo de busca

--- main.py
+++ main.py
@@ -23,14 +23,32 @@ print("Matriz Inicial:")
 scrambledBoard.showNodeMatrix()
 print("\n")
 
-print("####  INICIO BUSCA CEGA ####")
-print("\n")
-buscaCega.buscaCega()
-print("####   FIM BUSCA CEGA ####")
-print("####  INICIO BUSCA HEURISTICA ####")
-print("\n")
-buscaHeuristica.buscaHeuristica()
-print("####   FIM BUSCA HEURISTICA ####")
+opt = None
+while opt != 0:
+    try:
+        print("Escolha o algoritimo de busca: \n 1 - Busca Cega\n2 - Busca heuristica(Numero de passos até a posicao correta)\n0 - Finalizar")
+        opt = int(input())
+    except ValueError:
+        print("Você digitou um valor que não é um número!!!")
+    print("\n")
+    if opt == 1:
+        print("####  INICIO BUSCA CEGA ####")
+        print("\n")
+        buscaCega.buscaCega()
+        print("####   FIM BUSCA CEGA ####")
+    elif  opt == 2:
+        print("####  INICIO BUSCA HEURISTICA ####")
+        print("\n")
+        buscaHeuristica.buscaHeuristica()
+        print("####   FIM BUSCA HEURISTICA ####")
+
+    elif  opt == 0:
+        print("Fim")
+    else:
+        print("Você digitou uma opção fora dos limites!!")
+    print("\n")
+    print("\n")
+
 
 print("\n")
 print("#"*30)
