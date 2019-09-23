e6f89cdaf3663e6dc7c991c195c8e6461ad6432b
RenanJochem98 <renanjochem98@gmail.com>
2019-09-23 13:38:09 -0300

    Adicionando forma do usuario adicionar a matriz iinial

--- main.py
+++ main.py
@@ -8,9 +8,28 @@ print("#"*30)
 print(" "*7+"INICIANDO BUSCA")
 print("#"*30)
 print("\n")
+mat = ""
 
+while len(mat) != 9:
+    print("Digite o Jogo como deseja resolver:(No formato: 0,2,1,5,6...[separados por virgula e sem espaços em branco])(Zero representa a posicao vazia)")
+    mat = input()
+    mat.strip() #para retirar espacoes em brando na volta
+    mat = mat.split(",")
+    if len(mat) > 9:
+        print("Você digitou numeros a mais!!")
+    elif len(mat) < 9:
+        print("Você digitou números a menos!!!")
+
+matrix = []
+temp = []
+for i in mat:
+    temp.append(int(i))
+    if len(temp) == 3:
+        matrix.append(temp)
+        temp = []
 goalBoard = Board('goal')
-scrambledBoard = Board('scrambled')
+scrambledBoard = Board('matrix', matrix)
+
 buscaCega = BuscaCega(scrambledBoard, goalBoard)
 buscaHeuristica = BuscaHeuristica(scrambledBoard, goalBoard)
 
