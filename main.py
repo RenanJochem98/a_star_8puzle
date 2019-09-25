490b5573367d8b9f4a3154a5c7b1070d1ece4141
RenanJochem98 <renanjochem98@gmail.com>
2019-09-24 21:31:19 -0300

    Alterando arquivo inicial do programa

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from Board import Board
-# from SearchEngine import SearchEngine
-from BuscaCega import BuscaCega
-from BuscaHeuristica import BuscaHeuristica
+from BlindSearch import BlindSearch
+from TwoStepsSearch import TwoStepsSearch
+from OutOfPositionSearch import OutOfPositionSearch
 from Input import Input
+import time
 
 print("")
 print("#"*30)
@@ -11,27 +12,23 @@ print("#"*30)
 print("\n")
 
 newInput = Input()
-matrix =  newInput.pedeMatrizInicial()
-
+# matrix =  newInput.pedeMatrizInicial()
 goalBoard = Board('goal')
-scrambledBoard = Board('matrix', matrix)
+scrambledBoard = Board('scrambled')
 
-buscaCega = BuscaCega(scrambledBoard, goalBoard)
-buscaHeuristica = BuscaHeuristica(scrambledBoard, goalBoard)
-
-
-print("Matriz objetivo:")
-goalBoard.showNodeMatrix()
-print("\n")
-
-print("Matriz Inicial:")
-scrambledBoard.showNodeMatrix()
-print("\n")
+blindSearch = BlindSearch(scrambledBoard, goalBoard)
+outOfPosition = OutOfPositionSearch(scrambledBoard, goalBoard)
+twoSteps = TwoStepsSearch(scrambledBoard, goalBoard)
 
 opt = None
 while opt != 0:
     try:
-        print("Escolha o algoritimo de busca: \n 1 - Busca Cega\n2 - Busca heuristica(Numero de passos até a posicao correta)\n3 - Trocar tabuleiro\n0 - Finalizar")
+        print("Escolha o algoritimo de busca:")
+        print("1 - Busca Cega:")
+        print("2 - Busca Heuristica(Numero de passos até a posicao correta)(Busca Boa):")
+        print("3 - Busca Heuristica(Valores fora de posição)(Não tão boa):")
+        print("4 - Trocar tabuleiro")
+        print("0 - Finalizar")
         opt = int(input())
     except ValueError:
         print("Você digitou um valor que não é um número!!!")
@@ -39,30 +36,32 @@ while opt != 0:
     if opt == 1:
         print("####  INICIO BUSCA CEGA ####")
         print("\n")
-        buscaCega.buscaCega()
+        blindSearch.buscaCega()
         print("####   FIM BUSCA CEGA ####")
+        time.sleep(2)
     elif  opt == 2:
         print("####  INICIO BUSCA HEURISTICA ####")
         print("\n")
-        buscaHeuristica.buscaHeuristica()
+        twoSteps.buscaHeuristica()
         print("####   FIM BUSCA HEURISTICA ####")
+        time.sleep(2)
     elif  opt == 3:
+        print("####  INICIO BUSCA HEURISTICA ####")
+        print("\n")
+        outOfPosition.buscaHeuristica()
+        print("####   FIM BUSCA HEURISTICA ####")
+        time.sleep(2)
+    elif  opt == 4:
         matrix =  newInput.pedeMatrizInicial()
         scrambledBoard = Board('matrix', matrix)
         print("Matriz Inicial Alterado:")
         scrambledBoard.showNodeMatrix()
-        print("\n")
-
-
+        time.sleep(2)
     elif  opt == 0:
         print("Fim")
     else:
         print("Você digitou uma opção fora dos limites!!")
     print("\n")
-    print("\n")
-
-
-print("\n")
 print("#"*30)
 print(" "*4+"FINALIZANDO ALGORITMO")
 print("#"*30)
